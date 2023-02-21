bea5a5d83fd483461e1e9ef144d14068e11e179e
Arati Langote <aratilangote06@gmail.com>
2023-02-21 16:37:14 +0530

    NavigationDrawer

---
+++ app/src/main/java/com/technolite/connectit/HomeActivity.java
@@ -0,0 +1,86 @@
+package com.technolite.connectit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("ConnectIt");
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+
+        navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_premium:
+                Toast.makeText(this, "Premium", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_business_info:
+                Toast.makeText(this, "Business Info", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_wp_msg:
+                Toast.makeText(this, "Whatsapp msgs", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_sms_schedule:
+                Toast.makeText(this, "Sms scheduler", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_bulk_sms:
+                Toast.makeText(this, "Bulk msgs", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_permission:
+                Toast.makeText(this, "Permission", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_aboutUs:
+                Toast.makeText(this, "About us", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Help", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_referFriend:
+                Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logout", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
+    }
+}
--- app/src/main/java/com/technolite/connectit/MainActivity.java
+++ app/src/main/java/com/technolite/connectit/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         nextPage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, AllButtonsActivity.class));
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
             }
         });
     }
