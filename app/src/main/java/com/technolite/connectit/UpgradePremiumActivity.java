f6660fa09b8fa14b579c25b199265e4b117a552d
Rutujapatil20 <rutujapatil202001@gmail.com>
2023-02-28 12:51:22 +0530

    Commit

--- app/src/main/java/com/technolite/connectit/UpgradePremiumActivity.java
+++ app/src/main/java/com/technolite/connectit/UpgradePremiumActivity.java
@@ -2,13 +2,27 @@ package com.technolite.connectit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UpgradePremiumActivity extends AppCompatActivity {
+
+    Button plan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade_premium);
+
+        plan = findViewById(R.id.plan);
+        plan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent send = new Intent(UpgradePremiumActivity.this, UpgradePremiumActivity_2.class);
+                startActivity(send);
+            }
+        });
     }
 }
