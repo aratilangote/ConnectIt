3c91c6dca299163b264210870997677a3251c3bc
Arati Langote <aratilangote06@gmail.com>
2023-03-03 18:08:20 +0530

    second commit

--- app/src/main/java/com/technolite/connectit/HomeActivity.java
+++ app/src/main/java/com/technolite/connectit/HomeActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
@@ -13,13 +17,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.technolite.connectit.module.SlideItem;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -30,6 +38,10 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
     CardView BtnPremium, BtnNetworkSMS, BtnBithday, BtnSettings, BtnPermission, BtnBulkWhatsapp, BtnWhatsapp, BtnEazypost, BtnDownloads, btnbusiness;
 
+
+    ViewPager2 viewPager2;
+
+    private Handler slideHandler = new Handler();
 
     @SuppressLint({"ResourceAsColor", "MissingInflatedId"})
     @Override
@@ -142,9 +154,57 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         });
 
 
+        //Viewpager
+        viewPager2 = findViewById(R.id.view_pager);
+
+        List<SlideItem> slideItems = new ArrayList<>();
+        slideItems.add(new SlideItem(R.color.teal_200));
+        slideItems.add(new SlideItem(R.color.teal_700));
+        slideItems.add(new SlideItem(R.color.teal_200));
+        //slideItems.add(new SlideItem(R.drawable.image1));
+        //slideItems.add(new SlideItem(R.drawable.image2));
+        //slideItems.add(new SlideItem(R.drawable.image3));
+
+        viewPager2.setAdapter(new SlideAdapter(slideItems, viewPager2));
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(5);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(30));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+
+                float r = 1-Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f);
+
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+
+                slideHandler.removeCallbacks(slideRunnable);
+                slideHandler.postDelayed(slideRunnable, 2000);
+            }
+        });
+
+
 
 
     }
+    private Runnable slideRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem()+1);
+        }
+    };
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
